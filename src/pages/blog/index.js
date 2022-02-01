c0d8e1590bcfa120598a926a2035201d4a0ab553
ochi <ocos54ce@gmail.com>
2022-02-01 21:20:32 +0900

    feat: tutorial part6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({data}) => {
   return (
@@ -11,11 +10,12 @@ const BlogPage = ({data}) => {
           data.allMdx.nodes.map(node => {
             return (
               <article key={node.id}>
-                <h2>{node.frontmatter.title}</h2>
+                <h2>
+                  <Link to={`/blog/${node.slug}`}>
+                    {node.frontmatter.title}
+                  </Link>
+                </h2>
                 <p>Posted: {node.frontmatter.date}</p>
-                <MDXRenderer>
-                  {node.body}
-                </MDXRenderer>
               </article>
               )
           })
@@ -26,7 +26,7 @@ const BlogPage = ({data}) => {
 }
 
 export const query = graphql`
-query MyQuery {
+query {
   allMdx(sort: {fields: frontmatter___date, order: DESC}) {
     nodes {
       frontmatter {
@@ -34,7 +34,7 @@ query MyQuery {
         title
       }
       id
-      body
+      slug
     }
   }
 }
